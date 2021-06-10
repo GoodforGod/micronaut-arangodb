32449d96b933d3fca1ba9fffd635bef97459848d
Anton Kurako <goodforgod.dev@gmail.com>
2021-06-11 00:28:40 +0300

    [2.2.0-SNAPSHOT] ArangoSSLConfiguration introduced ArangoProperties added

---
+++ src/main/java/io/micronaut/configuration/arango/ArangoProperties.java
@@ -0,0 +1,20 @@
+package io.micronaut.configuration.arango;
+
+public interface ArangoProperties {
+
+    String HOSTS = "arangodb.hosts";
+    String HOST = "arangodb.host";
+    String PORT = "arangodb.port";
+    String USER = "arangodb.user";
+    String PASSWORD = "arangodb.password";
+    String TIMEOUT = "arangodb.timeout";
+    String PROTOCOL = "arangodb.protocol";
+    String USE_SSL = "arangodb.usessl";
+    String CHUNK_SIZE = "arangodb.chunksize";
+    String MAX_CONNECTIONS = "arangodb.connections.max";
+    String CONNECTION_TTL = "arangodb.connections.ttl";
+    String KEEP_ALIVE_INTERVAL = "arangodb.connections.keepAlive.interval";
+    String ACQUIRE_HOST_LIST = "arangodb.acquireHostList";
+    String ACQUIRE_HOST_LIST_INTERVAL = "arangodb.acquireHostList.interval";
+    String LOAD_BALANCING_STRATEGY = "arangodb.loadBalancingStrategy";
+}
---
+++ src/main/java/io/micronaut/configuration/arango/ArangoSSLConfiguration.java
@@ -0,0 +1,121 @@
+package io.micronaut.configuration.arango;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.exceptions.ConfigurationException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Base64;
+
+public class ArangoSSLConfiguration {
+
+    /**
+     * If set to true SSL will be used when connecting to an ArangoDB server.
+     * @see com.arangodb.ArangoDB.Builder#useSsl(Boolean)
+     */
+    private Boolean useSsl = false;
+
+    /**
+     * The name of the requested certificate type.
+     * @see CertificateFactory#getInstance(String)
+     */
+    private String certificateType = "X.509";
+    /**
+     * The standard name of the requested trust management algorithm.
+     * @see TrustManagerFactory#getInstance(String)
+     */
+    private String algorithmName = TrustManagerFactory.getDefaultAlgorithm();
+
+    /**
+     * Base64 encoded CA certificate.
+     */
+    private String certificate;
+
+    /**
+     * The alias name of certificate.
+     */
+    private String alias = "caCert";
+
+    /**
+     * The standard name of the requested protocol.
+     * @see javax.net.ssl.SSLContext#getInstance(String)
+     */
+    private String protocol = "TLS";
+
+    public SSLContext getSSLContext() {
+        try {
+            final byte[] certificateAsBytes = Base64.getDecoder().decode(getCertificate());
+            try (final InputStream is = new ByteArrayInputStream(certificateAsBytes)) {
+                final CertificateFactory factory = CertificateFactory.getInstance(getCertificateType());
+                final Certificate certificate = factory.generateCertificate(is);
+
+                final TrustManagerFactory managerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                final KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                keyStore.load(null);
+                keyStore.setCertificateEntry(getAlias(), certificate);
+
+                managerFactory.init(keyStore);
+
+                final SSLContext sslContext = SSLContext.getInstance(getProtocol());
+                sslContext.init(null, managerFactory.getTrustManagers(), null);
+                return sslContext;
+            }
+        } catch (Exception e) {
+            throw new ConfigurationException(e.getMessage());
+        }
+    }
+
+    public Boolean getUseSsl() {
+        return useSsl;
+    }
+
+    public void setUseSsl(Boolean useSsl) {
+        this.useSsl = useSsl;
+    }
+
+    public String getCertificateType() {
+        return certificateType;
+    }
+
+    public void setCertificateType(String certificateType) {
+        this.certificateType = certificateType;
+    }
+
+    public String getAlgorithmName() {
+        return algorithmName;
+    }
+
+    public void setAlgorithmName(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+}
